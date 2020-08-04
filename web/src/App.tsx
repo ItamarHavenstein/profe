bde7c94d311f7d3c3d29737eac451b62932c9f93
Itamar <itamarhavenstein@hotmail.com>
2020-08-03 22:19:07 -0300

    primeiro commit do projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from '../src/pages/Landing';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
