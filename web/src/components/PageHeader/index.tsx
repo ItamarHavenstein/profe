644d073e5040f66f2cb067f90ec5d2b44735148e
Itamar <itamarhavenstein@hotmail.com>
2020-08-06 22:03:37 -0300

    componente page header

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -8,25 +8,27 @@ import logoImg from '../../assets/images/logo.svg';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title,children}) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, description }) => {
     return (
-         <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
+        <header className="page-header">
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Voltar" />
+                </Link>
+                <img src={logoImg} alt="Proffy" />
+            </div>
 
-                <div className="header-content">
-                    <strong>
-                        {title}
-                    </strong>
-                    {children}
-                </div>
-            </header>
+            <div className="header-content">
+                <strong>
+                    {title}
+                </strong>
+                {description && <p>{description}</p>}
+                {children}
+            </div>
+        </header>
     );
 }
 
