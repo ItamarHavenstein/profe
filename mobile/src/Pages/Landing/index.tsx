97f4f8dc860714da83e4e41a9f7ad67bd9251114
Itamar <itamarhavenstein@hotmail.com>
2020-08-10 23:35:09 -0300

    inserção da quantidade de conexões que foram criadas

--- mobile/src/Pages/Landing/index.tsx
+++ mobile/src/Pages/Landing/index.tsx
@@ -8,9 +8,21 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import api from '../../services/api';
 
 function Landing() {
     const { navigate } = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const total = response.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
 
     function handleNavigationToGiveClassesPage() {
         navigate('GiveClasses');
@@ -42,14 +54,14 @@ function Landing() {
                 <RectButton
                     onPress={handleNavigationToGiveClassesPage}
                     style={[styles.button, styles.buttonSecondary]}>
-                        
+
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Dar aulas</Text>
                 </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
