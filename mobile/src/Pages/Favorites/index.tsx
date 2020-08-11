9615cc302f7b0dca116a218b6146dfd875f9f1e5
Itamar <itamarhavenstein@hotmail.com>
2020-08-10 23:34:27 -0300

    carrega a lista de profys que foram favoritados utilizando o AsyncStorage

--- mobile/src/Pages/Favorites/index.tsx
+++ mobile/src/Pages/Favorites/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState } from 'react';
+import { ScrollView, View } from 'react-native';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import styles from './styles';
+import {useFocusEffect} from '@react-navigation/native';
 
-function Favorites(){
-    return(
-           <View style={styles.container}>
-            <PageHeader title="Meus Proffys favoritos"/>
+function Favorites() {
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorite() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response);
+
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorite();
+    });
+
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Meus Proffys favoritos" />
+
+            <ScrollView style={styles.favoritesList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}>
+                    {favorites.map((teacher: Teacher) => {
+                        return (
+                            <TeacherItem 
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={true}
+                            />
+                        )
+                    })}
+            </ScrollView>
         </View>
     );
 }
